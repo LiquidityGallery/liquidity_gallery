a19af2278ec11eecbb78f09ce9abfd68c5bee746
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2021-06-16 22:58:25 +0800

    showmessage color added

--- lib/Functions/showMessage.dart
+++ lib/Functions/showMessage.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 /// Simplify ScaffoldMessage.of(context).showSnackBar()
 void showMessage(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$message')));
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(backgroundColor: Colors.cyan,content: Text('$message',style: TextStyle(color: Colors.white),)));
 }
