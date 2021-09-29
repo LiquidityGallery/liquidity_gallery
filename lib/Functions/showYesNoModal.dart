41408805bebc26c2bc180b10dab9d711eabc0696
fanchungchit <66772739+fanchungchit@users.noreply.github.com>
2021-09-29 11:22:01 +0800

    yes no modal update

--- lib/Functions/showYesNoModal.dart
+++ lib/Functions/showYesNoModal.dart
@@ -1,34 +1,46 @@
-
 import 'package:flutter/material.dart';
 
-Future <void> showYesNoModal(BuildContext context, void Function() onYes, {String? text})async {
- await showModalBottomSheet(
-    //   isScrollControlled: true,
-       shape: RoundedRectangleBorder(
-         borderRadius: BorderRadius.vertical(
-           top: Radius.circular(16),
-         ),
-       ),
-       clipBehavior: Clip.antiAliasWithSaveLayer,
-       context: context,
-       builder: (_) => Container(
-      //   height: MediaQuery.of(context).size.height * 0.92,
-         child: SafeArea(
-           child: Column(
-             mainAxisSize: MainAxisSize.min,
-             children: [
-ListTile(
-  title: Text(text==null ? "Do you want to continue?" : text) ),
-               ListTile(
-                 leading: Icon(Icons.check_circle,color: Colors.cyan[300],),
-                 title: Text("Yes"), onTap: onYes,),
-               ListTile(
-                 leading: Icon(Icons.cancel),
-                 title: Text("Cancel",),onTap: (){
-                   Navigator.pop(context);
-               },),
-           ],),
-         ),
-       ));
- }
-
+Future<void> showYesNoModal(BuildContext context, void Function() onYes,
+    {String? text}) async {
+  await showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      //   isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(16),
+        ),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      context: context,
+      builder: (_) => Container(
+        color: Colors.white,
+            //   height: MediaQuery.of(context).size.height * 0.92,
+            child: SafeArea(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                      title: Text(
+                          text == null ? "Do you want to continue?" : text)),
+                  ListTile(
+                    leading: Icon(
+                      Icons.check_circle,
+                      color: Colors.cyan[300],
+                    ),
+                    title: Text("Yes"),
+                    onTap: onYes,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.cancel),
+                    title: Text(
+                      "Cancel",
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ));
+}
