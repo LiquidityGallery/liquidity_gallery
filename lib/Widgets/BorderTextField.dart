7d733a362310bfb3f52774890ddc452768c892e0
fanchungchit <66772739+fanchungchit@users.noreply.github.com>
2021-10-04 15:22:17 +0800

    BorderTextField update
    + onTap

--- lib/Widgets/BorderTextField.dart
+++ lib/Widgets/BorderTextField.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 /// A custom textfield with Border on it.
 
 class BorderTextField extends StatelessWidget {
+  final GestureTapCallback? onTap;
   final bool? isinputnumber;
   final String? labeltext;
   final TextEditingController controller;
@@ -20,34 +21,48 @@ class BorderTextField extends StatelessWidget {
 
   const BorderTextField(
       {Key? key,
+      this.onTap,
       this.labeltext,
       required this.controller,
       this.isborder = true,
-      this.isinputnumber, this.bordercolor, this.hinttext, this.fillcolor, this.autofocus=false, this.radius=8.0, this.minLines=1, this.borderWidth=0.2, this.textStyle, this.onChanged, this.readOnly=false})
+      this.isinputnumber,
+      this.bordercolor,
+      this.hinttext,
+      this.fillcolor,
+      this.autofocus = false,
+      this.radius = 8.0,
+      this.minLines = 1,
+      this.borderWidth = 0.2,
+      this.textStyle,
+      this.onChanged,
+      this.readOnly = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-        var brightness = MediaQuery.of(context).platformBrightness;
-            bool darkModeOn = brightness == Brightness.dark;
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool darkModeOn = brightness == Brightness.dark;
     return Padding(
       padding: const EdgeInsets.all(2.0),
       child: TextField(
+        onTap: onTap,
         onChanged: onChanged,
-        style: textStyle ==null ? TextStyle(fontSize: 14) :textStyle,
+        style: textStyle == null ? TextStyle(fontSize: 14) : textStyle,
         autofocus: autofocus,
-       readOnly: readOnly,
-       minLines: minLines,
+        readOnly: readOnly,
+        minLines: minLines,
         maxLines: 10,
         keyboardType:
             isinputnumber == null ? TextInputType.text : TextInputType.number,
         controller: controller,
         decoration: InputDecoration(
           filled: true,
-          fillColor: fillcolor==null ? Colors.white : fillcolor,
+          fillColor: fillcolor == null ? Colors.white : fillcolor,
           enabledBorder: OutlineInputBorder(
             borderSide: isborder == true
-                ? BorderSide(color: bordercolor ==null ? Colors.black54 : bordercolor!, width: borderWidth)
+                ? BorderSide(
+                    color: bordercolor == null ? Colors.black54 : bordercolor!,
+                    width: borderWidth)
                 : BorderSide.none,
             borderRadius: BorderRadius.circular(radius),
           ),
