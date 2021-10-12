6b38194c0b630af9437ce4d8f7a4eba54065173d
samsiu200234 <samsiu200234@yahoo.com.hk>
2021-10-12 11:26:57 +0800

    showLockPanel

---
+++ lib/Functions/showLockPanel.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../liquidity_gallery.dart';
+
+Future<dynamic> showLockPanel(BuildContext context,
+    Function onPressed, String password) async {
+  TextEditingController _controller =TextEditingController();
+
+
+  return  await                                           showModal(
+      context,
+      Column(
+          mainAxisSize:
+          MainAxisSize.min,
+          children: [
+            BorderTextField(
+              controller: _controller,
+              labeltext: 'Password',
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ConfirmButton(
+                    text: 'ENTER',
+                    onPressed:
+                        () async {
+                      if (_controller
+                          .text ==
+                          password) {
+onPressed();
+                      } else {
+                        Navigator.pop(
+                            context);
+                        showMessage(
+                            context,
+                            'Password failed');
+                      }
+                    },
+                    radius: 0, color: Colors.grey[800],
+                  ),
+                ),
+              ],
+            )
+          ]),
+      radius: 0);
+}
+
