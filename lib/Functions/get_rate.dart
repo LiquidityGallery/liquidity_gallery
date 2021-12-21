529f73a12741e0d7cc6c18bdbeb8b7fcee5b967f
samsiu200234 <samsiu200234@yahoo.com.hk>
2021-12-21 11:28:53 +0800

    Update get_rate.dart

--- lib/Functions/get_rate.dart
+++ lib/Functions/get_rate.dart
@@ -9,11 +9,12 @@ class Forex {
   late Map<String, dynamic> _rates;
 
   Future<void> _fetchCurrencies() async {
-    print('fetching ');
     Uri baseUri = Uri.parse('http://www.convertmymoney.com/rates.json');
     final response = await http.get(baseUri, headers: {
-      "Accept": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
     });
     Map<String, dynamic> jsonResponse =
     json.decode(response.body) as Map<String, dynamic>;
