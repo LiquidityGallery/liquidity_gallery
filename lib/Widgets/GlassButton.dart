0b2fabb9f6903271ad3f18da4b9720a44e837837
Sam Siu <samsiu@Sams-Mac-mini.local>
2022-03-09 16:08:22 +0800

    update

--- lib/Widgets/GlassButton.dart
+++ lib/Widgets/GlassButton.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class GlassButton extends StatelessWidget {
-  final Widget? child;
+  final Function() onTap;
+  final Widget child;
   final double? radius;
   final Color? color;
   final double borderWidth;
 
-  const GlassButton({Key? key, this.child, this.radius, this.color, this.borderWidth=1}) : super(key: key);
+  const GlassButton({Key? key, required this.child, this.radius, this.color, this.borderWidth=1, required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Padding(
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(child: Padding(
       padding: const EdgeInsets.only(top: 8,bottom: 8,right: 12,left: 12),
       child:   child==null ? Container() : child!,
-    ),decoration: BoxDecoration(borderRadius: BorderRadius.circular(radius==null ? 10 : radius!),border: Border.all(width: borderWidth, color: color ?? Colors.grey)),);
+    ),decoration: BoxDecoration(borderRadius: BorderRadius.circular(radius==null ? 10 : radius!),border: Border.all(width: borderWidth, color: color ?? Colors.grey)),));
 
   }
 }
