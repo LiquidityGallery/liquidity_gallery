e9b462ffaab2baf8e0d20f7d5af24b01fbdfd92e
fanchungchit <66772739+fanchungchit@users.noreply.github.com>
2022-04-04 09:09:50 +0800

    remove unused pub

--- lib/Widgets/NavBar.dart
+++ lib/Widgets/NavBar.dart
@@ -1,41 +1,39 @@
-
-
-
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:liquidity_gallery/liquidity_gallery.dart';
-
-
-
 
 ///The number of widget and tab names should be equal
 class NavBar extends StatefulWidget {
-final List<Widget> children;
-final List<BottomNavigationBarItem> items;
-final num NumOfItem;
-final Color? color;
-final double unselectedFontSize;
-final double selectedFontSize;
+  final List<Widget> children;
+  final List<BottomNavigationBarItem> items;
+  final num NumOfItem;
+  final Color? color;
+  final double unselectedFontSize;
+  final double selectedFontSize;
 
-NavBar({Key? key,required this.NumOfItem,required this.children, required this.items, this.color, this.unselectedFontSize=12, this.selectedFontSize=14}) : super(key: key);
+  NavBar(
+      {Key? key,
+      required this.NumOfItem,
+      required this.children,
+      required this.items,
+      this.color,
+      this.unselectedFontSize = 12,
+      this.selectedFontSize = 14})
+      : super(key: key);
 
   @override
   _NavBarState createState() => _NavBarState();
-
-
 }
 
 class _NavBarState extends State<NavBar> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  late List<Widget> _widgetOptions ;
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  late List<Widget> _widgetOptions;
 
   @override
   void initState() {
     super.initState();
-    _widgetOptions= widget.children;
+    _widgetOptions = widget.children;
   }
 
   void _onItemTapped(int index) {
@@ -55,14 +53,13 @@ class _NavBarState extends State<NavBar> {
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: widget.color== null ? Colors.cyan[800] : widget.color,
+        backgroundColor: widget.color == null ? Colors.cyan[800] : widget.color,
         items: widget.items,
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.white,
         selectedFontSize: widget.selectedFontSize,
         unselectedFontSize: widget.unselectedFontSize,
         unselectedItemColor: Colors.white70,
-
         onTap: _onItemTapped,
         type: BottomNavigationBarType.fixed,
       ),
