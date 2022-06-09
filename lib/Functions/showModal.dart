449c04acc6ca2e76252af75246629c14b8fed4e4
fanchungchit <66772739+fanchungchit@users.noreply.github.com>
2022-06-09 15:09:18 +0800

    update

--- lib/Functions/showModal.dart
+++ lib/Functions/showModal.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
-Future<dynamic> showModal(BuildContext context,
-    Widget child, {double? radius, bool? isDismissible}) async {
-  return  await showModalBottomSheet(
-      isDismissible: isDismissible==null? true: isDismissible,
+Future<dynamic> showModal(BuildContext context, Widget child,
+    {double? radius, bool? isDismissible}) async {
+  return await showModalBottomSheet(
+      isDismissible: isDismissible == null ? true : isDismissible,
       isScrollControlled: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
-          top: Radius.circular(radius==null ? 16: radius),
+          top: Radius.circular(radius == null ? 16 : radius),
         ),
       ),
       clipBehavior: Clip.antiAliasWithSaveLayer,
       context: context,
-      builder: (context) =>
-          Padding(
+      builder: (context) => Padding(
             padding: MediaQuery.of(context).viewInsets,
             child: Container(
               //   height: MediaQuery.of(context).size.height * 0.92,
@@ -22,26 +21,22 @@ Future<dynamic> showModal(BuildContext context,
           ));
 }
 
-Future<dynamic> showModalMax(BuildContext context,
-    Widget child, {double? radius, num height=0.92,bool? isDismissible}) async {
-  return await showModalBottomSheet(
-      isDismissible: isDismissible==null? true: isDismissible,
+Future<T?> showModalMax<T>(BuildContext context, Widget child,
+    {double? radius, num height = 0.92, bool? isDismissible}) async {
+  return await showModalBottomSheet<T>(
+      isDismissible: isDismissible == null ? true : isDismissible,
       isScrollControlled: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
-          top: Radius.circular(radius==null ? 16: radius),
+          top: Radius.circular(radius == null ? 16 : radius),
         ),
       ),
       clipBehavior: Clip.antiAliasWithSaveLayer,
       context: context,
-      builder: (context) =>
-          Padding(
+      builder: (context) => Padding(
             padding: MediaQuery.of(context).viewInsets,
             child: Container(
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .height * height,
+              height: MediaQuery.of(context).size.height * height,
               child: child,
             ),
           ));
