fe737b15190fd54fe76117bc03b5ca2cbe74546f
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2023-05-18 18:48:42 +0800

    update

--- lib/Functions/timeDifference.dart
+++ lib/Functions/timeDifference.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 
-String timeDifference(DateTime timestamp, DateTime? secondTimestamp) {
+num timeDifference(DateTime timestamp) {
   late DateTime secondDate;
-  if (secondTimestamp == null) {
     secondDate = DateTime(
       DateTime.now().year,
       DateTime.now().month,
       DateTime.now().day,
     );
-  } else {
-    secondDate = secondTimestamp;
-  }
+
 
   final different = DateTime(timestamp.year, timestamp.month, timestamp.day)
       .difference(secondDate);
 
-  final days = different == const Duration() ? '' : '${different.inDays}';
-  final differentString = days.isEmpty
-      ? '今日'
-      : (different == const Duration(days: 1) ? ' 明天' : ' 餘下$days天');
-  return differentString;
+  final num days = num.parse(different == const Duration() ? '' : '${different.inDays}');
+  // final differentString = days.isEmpty
+  //     ? '今日'
+  //     : (different == const Duration(days: 1) ? ' 明天' : ' 餘下$days天');
+  return days;
 }
