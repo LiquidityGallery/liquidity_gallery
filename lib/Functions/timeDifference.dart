199af7d28d199af3ed2f22a31c9272c2282af1a4
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2023-05-19 16:22:55 +0800

    fix

--- lib/Functions/timeDifference.dart
+++ lib/Functions/timeDifference.dart
@@ -12,7 +12,7 @@ num timeDifference(DateTime timestamp) {
   final different = DateTime(timestamp.year, timestamp.month, timestamp.day)
       .difference(secondDate);
 
-  final num days = num.parse(different == const Duration() ? '' : '${different.inDays}');
+  final num days = num.tryParse(different == const Duration() ? '' : '${different.inDays}')?? 0;
   // final differentString = days.isEmpty
   //     ? '今日'
   //     : (different == const Duration(days: 1) ? ' 明天' : ' 餘下$days天');
