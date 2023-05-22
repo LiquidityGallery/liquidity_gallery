8b1c61d2a559ed06c8416946f1fa4cb66a58d4ee
Tony Fan <fanchungchit@icloud.com>
2023-05-22 12:16:18 +0800

    Remove flutter_datetime_picker

--- lib/Functions/datePicker.dart
+++ lib/Functions/datePicker.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-import 'package:intl/intl.dart';
 
 Future<DateTime?> datePicker(BuildContext context,
     {DateTime? minTime, DateTime? maxTime, DateTime? currentTime}) async {
-  return await DatePicker.showDatePicker(context,
-      showTitleActions: true,
-      minTime: minTime ?? DateTime(2020),
-      maxTime: maxTime ?? DateTime(2100),
-      onConfirm: (dateTime) =>
-          DateTime.parse(DateFormat('yyyy-MM-dd').format(dateTime)),
-      currentTime: currentTime ?? DateTime.now());
+  return await showDatePicker(
+      context: context,
+      initialDate: currentTime ?? DateTime.now(),
+      firstDate: minTime ?? DateTime(2020),
+      lastDate: maxTime ?? DateTime(2100));
+  // return await DatePicker.showDatePicker(context,
+  //     showTitleActions: true,
+  //     minTime: minTime ?? DateTime(2020),
+  //     maxTime: maxTime ?? DateTime(2100),
+  //     onConfirm: (dateTime) =>
+  //         DateTime.parse(DateFormat('yyyy-MM-dd').format(dateTime)),
+  //     currentTime: currentTime ?? DateTime.now());
 }
